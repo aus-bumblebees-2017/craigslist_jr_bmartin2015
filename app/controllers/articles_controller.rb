ed03a7890b617699dc2aa5a121a00f0bb8534aff
Ben Martin <email": null>
2017-05-08 16:26:43 -0500

    Add navbar

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,9 @@ class ArticlesController < ApplicationController
   def create
     @category = Category.find_by(id: params[:category_id])
     @article = @category.articles.new(article_params)
+    if helpers.current_user
+      @article.user = helpers.current_user
+    end
     if @article.save
       session[:edit_code] = @article.edit_id
       redirect_to category_article_url(@category, @article)
