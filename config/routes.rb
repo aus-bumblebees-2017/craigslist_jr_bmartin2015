51f010a7acd7a3f57a2072ce4b5b6b7d9a3f4771
Benjamin Martin <benjamin.martin2010@gmail.com>
2017-05-10 01:41:37 +0000

    Changed to manual routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :categories, only: [:index] do
-    resources :articles, except: [:edit]
-  end
+  # resources :categories, only: [:index] do
+  #   resources :articles, except: [:edit]
+  # end
 
-  resources :users, only: [:show, :create, :new]
-
+  get '/categories', to: 'categories#index'
+  get '/categories/:category_id/articles', to: 'articles#index', as: 'category_articles'
+  post '/categories/:category_id/articles', to: 'articles#create'
+  get '/categories/:category_id/articles/new', to: 'articles#new', as: 'new_category_article'
+  get '/categories/:category_id/articles/:id', to: 'articles#show', as: 'category_article'
+  patch '/categories/:category_id/articles/:id', to: 'articles#update'
+  put '/categories/:category_id/articles/:id', to: 'articles#update'
+  delete '/categories/:category_id/articles/:id', to: 'articles#destory'
   get '/categories/:category_id/articles/:id/edit/:code', to: 'articles#edit', as: 'edit_category_article'
+
+  post '/users', to: 'users#create', as: 'users'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/new', to: 'users#new', as: 'new_user'
+
+  # resources :users, only: [:show, :create, :new]
 
   get    '/login',  to: 'sessions#new'
   post   '/login',  to: 'sessions#create'
